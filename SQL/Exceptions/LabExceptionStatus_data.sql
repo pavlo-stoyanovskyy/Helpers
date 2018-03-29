1b2720891d3a71cb45c9a15f5050b6c98f06ac84
Pavlo Stoyanovskyy <pavlo.stoyanovskyy@gmail.com>
2018-03-29 16:53:05 -0400

    Create LabExceptionStatus_data.sql

---
+++ SQL/Exceptions/LabExceptionStatus_data.sql
@@ -0,0 +1,115 @@
+SET IDENTITY_INSERT [dbo].[LabExceptionEventType] OFF
+GO
+INSERT [dbo].[LabExceptionReason] ([LabExceptionReasonID], [Name], [CreatedByLoadID]) VALUES (1, N'Add/Delete Test Code', NULL)
+GO
+INSERT [dbo].[LabExceptionReason] ([LabExceptionReasonID], [Name], [CreatedByLoadID]) VALUES (2, N'Additional Specimen Needed', NULL)
+GO
+INSERT [dbo].[LabExceptionReason] ([LabExceptionReasonID], [Name], [CreatedByLoadID]) VALUES (3, N'Block Retrieval Request Cancellation (Specimen Not Being Sent)', NULL)
+GO
+INSERT [dbo].[LabExceptionReason] ([LabExceptionReasonID], [Name], [CreatedByLoadID]) VALUES (4, N'Clarify Test', NULL)
+GO
+INSERT [dbo].[LabExceptionReason] ([LabExceptionReasonID], [Name], [CreatedByLoadID]) VALUES (5, N'Contaminated POC', NULL)
+GO
+INSERT [dbo].[LabExceptionReason] ([LabExceptionReasonID], [Name], [CreatedByLoadID]) VALUES (6, N'Demo Change', NULL)
+GO
+INSERT [dbo].[LabExceptionReason] ([LabExceptionReasonID], [Name], [CreatedByLoadID]) VALUES (7, N'Gender Inquiry', NULL)
+GO
+INSERT [dbo].[LabExceptionReason] ([LabExceptionReasonID], [Name], [CreatedByLoadID]) VALUES (8, N'Gestational age for Amniotic fluid and CVS samples', NULL)
+GO
+INSERT [dbo].[LabExceptionReason] ([LabExceptionReasonID], [Name], [CreatedByLoadID]) VALUES (9, N'Indication Question', NULL)
+GO
+INSERT [dbo].[LabExceptionReason] ([LabExceptionReasonID], [Name], [CreatedByLoadID]) VALUES (10, N'Indication Question (GDX)', NULL)
+GO
+INSERT [dbo].[LabExceptionReason] ([LabExceptionReasonID], [Name], [CreatedByLoadID]) VALUES (11, N'Insufficient specimen (low specimen volume, clotted specimen, etc.) with high risk of test failure', NULL)
+GO
+INSERT [dbo].[LabExceptionReason] ([LabExceptionReasonID], [Name], [CreatedByLoadID]) VALUES (12, N'Medical Records', NULL)
+GO
+INSERT [dbo].[LabExceptionReason] ([LabExceptionReasonID], [Name], [CreatedByLoadID]) VALUES (13, N'Missing Patient DOB', NULL)
+GO
+INSERT [dbo].[LabExceptionReason] ([LabExceptionReasonID], [Name], [CreatedByLoadID]) VALUES (14, N'Missing Patient''s Name', NULL)
+GO
+INSERT [dbo].[LabExceptionReason] ([LabExceptionReasonID], [Name], [CreatedByLoadID]) VALUES (15, N'Missing specimen collection date and time', NULL)
+GO
+INSERT [dbo].[LabExceptionReason] ([LabExceptionReasonID], [Name], [CreatedByLoadID]) VALUES (16, N'Missing specimen source', NULL)
+GO
+INSERT [dbo].[LabExceptionReason] ([LabExceptionReasonID], [Name], [CreatedByLoadID]) VALUES (17, N'No Req Provided (GDX)', NULL)
+GO
+INSERT [dbo].[LabExceptionReason] ([LabExceptionReasonID], [Name], [CreatedByLoadID]) VALUES (18, N'Sample Issue (GDX)', NULL)
+GO
+INSERT [dbo].[LabExceptionReason] ([LabExceptionReasonID], [Name], [CreatedByLoadID]) VALUES (19, N'Slide & Block Request', NULL)
+GO
+INSERT [dbo].[LabExceptionReason] ([LabExceptionReasonID], [Name], [CreatedByLoadID]) VALUES (20, N'Source Verification', NULL)
+GO
+INSERT [dbo].[LabExceptionReason] ([LabExceptionReasonID], [Name], [CreatedByLoadID]) VALUES (21, N'Specimen Leaked', NULL)
+GO
+INSERT [dbo].[LabExceptionReason] ([LabExceptionReasonID], [Name], [CreatedByLoadID]) VALUES (22, N'Specimen Needed', NULL)
+GO
+INSERT [dbo].[LabExceptionReason] ([LabExceptionReasonID], [Name], [CreatedByLoadID]) VALUES (23, N'Test Cancellation Notice', NULL)
+GO
+INSERT [dbo].[LabExceptionReason] ([LabExceptionReasonID], [Name], [CreatedByLoadID]) VALUES (24, N'Test Clarification', NULL)
+GO
+INSERT [dbo].[LabExceptionReason] ([LabExceptionReasonID], [Name], [CreatedByLoadID]) VALUES (25, N'Test Failure', NULL)
+GO
+INSERT [dbo].[LabExceptionReason] ([LabExceptionReasonID], [Name], [CreatedByLoadID]) VALUES (26, N'Testing Indication Question doesn''t Match (GDX)', NULL)
+GO
+INSERT [dbo].[LabExceptionReason] ([LabExceptionReasonID], [Name], [CreatedByLoadID]) VALUES (27, N'Testing Question (GDX)', NULL)
+GO
+INSERT [dbo].[LabExceptionReason] ([LabExceptionReasonID], [Name], [CreatedByLoadID]) VALUES (28, N'Verification/Missing Data Request', NULL)
+GO
+INSERT [dbo].[LabExceptionReason] ([LabExceptionReasonID], [Name], [CreatedByLoadID]) VALUES (101, N'Revised Report', NULL)
+GO
+INSERT [dbo].[LabExceptionReason] ([LabExceptionReasonID], [Name], [CreatedByLoadID]) VALUES (102, N'Revised Report with Addendum', NULL)
+GO
+INSERT [dbo].[LabExceptionReason] ([LabExceptionReasonID], [Name], [CreatedByLoadID]) VALUES (103, N'Miscellaneous', 1)
+GO
+INSERT [dbo].[LabExceptionReason] ([LabExceptionReasonID], [Name], [CreatedByLoadID]) VALUES (104, N'Lab Problem', 19)
+GO
+INSERT [dbo].[LabExceptionReason] ([LabExceptionReasonID], [Name], [CreatedByLoadID]) VALUES (201, N'No Test Ordered', NULL)
+GO
+INSERT [dbo].[LabExceptionReason] ([LabExceptionReasonID], [Name], [CreatedByLoadID]) VALUES (202, N'Non BioReference Requistion', NULL)
+GO
+INSERT [dbo].[LabExceptionReason] ([LabExceptionReasonID], [Name], [CreatedByLoadID]) VALUES (203, N'Can Not Perform Test', NULL)
+GO
+INSERT [dbo].[LabExceptionReason] ([LabExceptionReasonID], [Name], [CreatedByLoadID]) VALUES (204, N'Req, No Specimen', NULL)
+GO
+INSERT [dbo].[LabExceptionReason] ([LabExceptionReasonID], [Name], [CreatedByLoadID]) VALUES (205, N'Specimen, No Req', NULL)
+GO
+INSERT [dbo].[LabExceptionReason] ([LabExceptionReasonID], [Name], [CreatedByLoadID]) VALUES (206, N'Test, No Specimen Sent', NULL)
+GO
+INSERT [dbo].[LabExceptionReason] ([LabExceptionReasonID], [Name], [CreatedByLoadID]) VALUES (207, N'Request Not Clear', NULL)
+GO
+INSERT [dbo].[LabExceptionReason] ([LabExceptionReasonID], [Name], [CreatedByLoadID]) VALUES (208, N'Inappropriate Specimen', NULL)
+GO
+INSERT [dbo].[LabExceptionReason] ([LabExceptionReasonID], [Name], [CreatedByLoadID]) VALUES (209, N'Names Do Not Match', NULL)
+GO
+INSERT [dbo].[LabExceptionReason] ([LabExceptionReasonID], [Name], [CreatedByLoadID]) VALUES (210, N'Miscellaneous', NULL)
+GO
+INSERT [dbo].[LabExceptionReason] ([LabExceptionReasonID], [Name], [CreatedByLoadID]) VALUES (211, N'Lab Problem', NULL)
+GO
+INSERT [dbo].[LabExceptionReason] ([LabExceptionReasonID], [Name], [CreatedByLoadID]) VALUES (212, N'Prenatal Review', NULL)
+GO
+INSERT [dbo].[LabExceptionReason] ([LabExceptionReasonID], [Name], [CreatedByLoadID]) VALUES (213, N'SpecialCoag', NULL)
+GO
+INSERT [dbo].[LabExceptionReason] ([LabExceptionReasonID], [Name], [CreatedByLoadID]) VALUES (214, N'Incorrect Fixative', NULL)
+GO
+INSERT [dbo].[LabExceptionReason] ([LabExceptionReasonID], [Name], [CreatedByLoadID]) VALUES (215, N'Site Mismatch', NULL)
+GO
+INSERT [dbo].[LabExceptionReason] ([LabExceptionReasonID], [Name], [CreatedByLoadID]) VALUES (216, N'IC Review', NULL)
+GO
+INSERT [dbo].[LabExceptionReason] ([LabExceptionReasonID], [Name], [CreatedByLoadID]) VALUES (217, N'No Tissue In Container', NULL)
+GO
+INSERT [dbo].[LabExceptionReason] ([LabExceptionReasonID], [Name], [CreatedByLoadID]) VALUES (218, N'Pre-Approval Hold', NULL)
+GO
+INSERT [dbo].[LabExceptionReason] ([LabExceptionReasonID], [Name], [CreatedByLoadID]) VALUES (219, N'4K Review', NULL)
+GO
+INSERT [dbo].[LabExceptionReason] ([LabExceptionReasonID], [Name], [CreatedByLoadID]) VALUES (220, N'Frequency Exceeded', NULL)
+GO
+INSERT [dbo].[LabExceptionReason] ([LabExceptionReasonID], [Name], [CreatedByLoadID]) VALUES (221, N'Revised Report', 1)
+GO
+INSERT [dbo].[LabExceptionReason] ([LabExceptionReasonID], [Name], [CreatedByLoadID]) VALUES (301, N'Test Not Performed (TNP)', NULL)
+GO
+INSERT [dbo].[LabExceptionReason] ([LabExceptionReasonID], [Name], [CreatedByLoadID]) VALUES (302, N'Quantity Not Sufficient (QNS)', NULL)
+GO
+INSERT [dbo].[LabExceptionReason] ([LabExceptionReasonID], [Name], [CreatedByLoadID]) VALUES (303, N'Alternate Test Performed (ATP)', NULL)
+GO
+SET IDENTITY_INSERT [dbo].[LabExceptionStatus] ON 
